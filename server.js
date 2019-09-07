3440fa47b22699dccd6000dd97bc34e0cd1021c2
unknown <seanhill2016@gmail.com>
2019-09-06 20:04:37 -0400

    setup

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+// Bodyparser Middleware
+app.use(express.json());
 
-  res.json(customers);
+// DB Config
+const db = config.get('mongoURI');
+
+// Connect to Mongo
+mongoose
+  .connect(db, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+
+const Item = require("./models/Item");
+
+app.post('/' , (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
+
 });
+
+// app.get('/api/customers', (req, res) => {
+//   const customers = [
+//     {id: 1, firstName: 'John', lastName: 'Doe'},
+//     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+//     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+//   ];
+
+//   res.json(customers);
+// });
 
 const port = 5000;
 
